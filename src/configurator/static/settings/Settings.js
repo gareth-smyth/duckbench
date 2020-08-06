551ddea8f5188158e2b4c952a6c25d6a889c36be
Gareth Smyth <gareth-smyth@users.noreply.github.com>
2020-08-06 23:38:05 +0100

    Collapsible settings.

--- src/configurator/static/settings/Settings.js
+++ src/configurator/static/settings/Settings.js
@@ -8,18 +8,24 @@ export default class Settings {
             }
             const currentSettings = node.attrs.currentSettings[plugin.name];
             return [
-                m('.h4', plugin.label),
-                ...plugin.settings.map(setting => {
-                    let currentSetting = currentSettings.find(currentSetting => currentSetting.name === setting.name);
-                    if(!currentSetting) {
-                        currentSetting = {
-                            name: setting.name,
-                            value: '',
+                m('.container', [ m('.row', [
+                    m('h4.col-9.my-auto', plugin.label),
+                    m('a.btn.btn-info.ml-3.my-auto.col-1', {'data-toggle': 'collapse', 'data-target': `#setting_${plugin.name}`}, [
+                        m('img', { src: "./images/gear-fill.svg", width:"23", height:"23", title:"Configure"}),
+                    ]),
+                ])]),
+                m('.mt-2', [
+                    ...plugin.settings.map(setting => {
+                        let currentSetting = currentSettings.find(currentSetting => currentSetting.name === setting.name);
+                        if(!currentSetting) {
+                            currentSetting = {
+                                name: setting.name,
+                                value: '',
+                            }
+                            currentSettings.push(currentSetting);
                         }
-                        currentSettings.push(currentSetting);
-                    }
-                    return m('.container-fluid', [m(Setting, {plugin, setting, currentSetting})]);
-                }),
+                        return m('.container-fluid.collapse', {id: `setting_${plugin.name}`}, [m(Setting, {plugin, setting, currentSetting})]);
+                })]),
             ];
         });
     }
