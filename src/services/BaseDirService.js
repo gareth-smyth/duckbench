ec593e18c6480a6255438b73959db09d8f5be4be
Gareth Smyth <gareth-smyth@users.noreply.github.com>
2021-02-04 20:17:38 +0000

    Fix for missing cache directory.

--- src/services/BaseDirService.js
+++ src/services/BaseDirService.js
@@ -1,6 +1,15 @@
 /* istanbul ignore file */
+const fs = require('fs');
 const path = require('path');
 
 global.BASE_DIR = path.join(__dirname, '../../');
 global.TOOLS_DIR = path.join(global.BASE_DIR, 'external_tools');
 global.CACHE_DIR = path.join(global.BASE_DIR, 'cache');
+
+if (!fs.existsSync(global.CACHE_DIR)) {
+    fs.mkdirSync(global.CACHE_DIR);
+}
+
+if (!fs.existsSync(global.TOOLS_DIR)) {
+    fs.mkdirSync(global.TOOLS_DIR);
+}
