239a8a90db58a4347153c56116580f5b62631c2c
Gareth Smyth <gareth-smyth@users.noreply.github.com>
2025-03-13 20:57:38 +0000

    Attempting to stop writing files as part of the tests

--- test/setup.ts
+++ test/setup.ts
@@ -5,6 +5,26 @@ import { vi } from "vitest";
 import "../src/services/BaseDirService";
 import "vitest-fetch-mock";
 
+vi.mock("fs", async (importOriginal) => {
+  const actual = await importOriginal();
+  const fakeFs = {
+    // @ts-expect-error Spread operator usage
+    ...actual,
+    chmodSync: vi.fn(),
+    copyFileSync: vi.fn(),
+    mkdirSync: vi.fn(),
+    rmdirSync: vi.fn(),
+    unlinkSync: vi.fn(),
+    writeFileSync: vi.fn(),
+    writeSync: vi.fn(),
+  };
+  return {
+    // @ts-expect-error Spread operator usage
+    ...actual,
+    default: fakeFs,
+    ...fakeFs,
+  };
+});
 const fetchMocker = createFetchMock(vi);
 
 // sets globalThis.fetch and globalThis.fetchMock to our mocked version
